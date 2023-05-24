8caa32e4afec98504b51c0257b4c9b75275e777e
Alistair Keiller <43255248+AlistairKeiller@users.noreply.github.com>
2023-05-24 14:24:49 -0700

    update

--- zsh.sh
+++ zsh.sh
@@ -3,7 +3,7 @@ set -e
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 # Install Programs
 /opt/homebrew/bin/brew install rustup-init gitui fish helix
-/opt/homebrew/bin/brew --cask julia discord skype
+/opt/homebrew/bin/brew install --cask julia discord skype
 /opt/homebrew/bin/brew install --cask alacritty --no-quarantine
 # Change Defalt Shell To Fish
 echo /opt/homebrew/bin/fish | sudo tee -a /etc/shells
