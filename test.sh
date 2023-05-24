f6e1014475325975182e1c1443d5b66712b410f6
Alistair Keiller <43255248+AlistairKeiller@users.noreply.github.com>
2023-05-24 13:42:30 -0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@ set -e
 # Install Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 # Install Programs
-/opt/homebrew/bin/brew install julia rustup-init python pango ffmpeg python-lsp-server texlive gitui fish helix discord skype
+/opt/homebrew/bin/brew install julia rustup-init gitui fish helix discord skype
 /opt/homebrew/bin/brew install alacritty --no-quarantine
 # Change Defalt Shell To Fish
 echo /opt/homebrew/bin/fish | sudo tee -a /etc/shells
