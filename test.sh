f75b7514936029a1ebb4d0ed98f3ad7ea2f22342
Alistair Keiller <43255248+AlistairKeiller@users.noreply.github.com>
2023-05-24 13:37:47 -0700

    Update test.sh

--- test.sh
+++ test.sh
@@ -7,36 +7,5 @@ set -e
 # Change Defalt Shell To Fish
 echo /opt/homebrew/bin/fish | sudo tee -a /etc/shells
 chsh -s /opt/homebrew/bin/fish
-# Add Homebrew And Rust To Fish Path
-fish_add_path /opt/homebrew/bin ~/.cargo/bin/ /usr/local/texlive/2023/bin/universal-darwin $(brew --prefix python)/libexec/bin
-# Install Rust
-rustup-init -y --profile complete
-cargo install --git https://github.com/wgsl-analyzer/wgsl-analyzer wgsl_analyzer
-# Install Pluto
-julia -e 'using Pkg;Pkg.add("Pluto")'
-# Configure Git
-git config --global user.name "Alistair Keiller"
-git config --global user.email alistair@keiller.net
-git config --global credential.helper osxkeychain
-# Configure Alacritty
-echo 'font:
-  size: 15.0
-window:
-  option_as_alt: Both'> ~/.config/alacritty/alacritty.yml
-# Configure Helix
-echo 'theme = "onedark"
-
-[editor]
-auto-save = true
-idle-timeout = 0
-line-number = "relative"
-completion-replace = true
-
-[editor.lsp]
-display-inlay-hints = true
-
-[editor.file-picker]
-hidden = false' > ~/.config/helix/config.toml
-# Add alistair directory to favorites in finder
-# Install Office 365 and grammarly from app store
-# Install Ti student software from their website
+# Run The Rest in Fish Shell
+fish test2.sh
