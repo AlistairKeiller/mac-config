c878a24a334501bcfd899cfb8c390832cafa1856
Alistair Keiller <43255248+AlistairKeiller@users.noreply.github.com>
2023-05-24 13:48:34 -0700

    change julia to cask

--- test.sh
+++ test.sh
@@ -2,8 +2,9 @@ set -e
 # Install Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 # Install Programs
-/opt/homebrew/bin/brew install julia rustup-init gitui fish helix discord skype
-/opt/homebrew/bin/brew install alacritty --no-quarantine
+/opt/homebrew/bin/brew install rustup-init gitui fish helix
+/opt/homebrew/bin/brew --cask julia discord skype
+/opt/homebrew/bin/brew install --cask alacritty --no-quarantine
 # Change Defalt Shell To Fish
 echo /opt/homebrew/bin/fish | sudo tee -a /etc/shells
 chsh -s /opt/homebrew/bin/fish
