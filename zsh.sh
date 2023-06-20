d204e1487edef29297e9d548a9325a3de9c95dbf
Alistair Keiller <43255248+AlistairKeiller@users.noreply.github.com>
2023-06-20 11:33:24 -0700

    use the brew git

--- zsh.sh
+++ zsh.sh
@@ -4,7 +4,7 @@ set -e
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # Install Programs
-/opt/homebrew/bin/brew install rustup-init gitui nushell helix python python-lsp-server
+/opt/homebrew/bin/brew install rustup-init gitui nushell helix python python-lsp-server git
 /opt/homebrew/bin/brew install --cask julia discord skype
 /opt/homebrew/bin/brew install --cask alacritty --no-quarantine
 
