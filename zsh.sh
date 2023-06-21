7ef3a4d87c9648cbb51a4a98fb88b68ca815a6b3
Alistair Keiller <43255248+AlistairKeiller@users.noreply.github.com>
2023-06-21 10:23:55 -0700

    Update zsh.sh

--- zsh.sh
+++ zsh.sh
@@ -4,7 +4,7 @@ set -e
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # Install Programs
-/opt/homebrew/bin/brew install rustup-init gitui nushell helix python python-lsp-server git
+/opt/homebrew/bin/brew install rustup-init gitui nushell helix python python-lsp-server
 /opt/homebrew/bin/brew install --cask julia discord skype
 /opt/homebrew/bin/brew install --cask alacritty --no-quarantine
 
@@ -43,3 +43,5 @@ display-inlay-hints = true
 [editor.file-picker]
 hidden = false' > ~/.config/helix/config.toml
 
+# Configure Nu
+echo 'let-env PATH = "/bin:/usr/bin:/opt/homebrew/bin"' >> "/Users/alistairkeiller/Library/Application Support/nushell/config.nu"
