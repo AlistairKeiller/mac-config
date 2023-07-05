f3ce23df736a20aa0caa3fb85aa9bef7d4f8e2a5
Alistair Keiller <43255248+AlistairKeiller@users.noreply.github.com>
2023-07-05 16:52:40 -0700

    Update zsh.sh

--- zsh.sh
+++ zsh.sh
@@ -4,7 +4,7 @@ set -e
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # Install Programs
-/opt/homebrew/bin/brew install rustup-init gitui nushell helix python python-lsp-server gh
+/opt/homebrew/bin/brew install rustup-init gitui nushell helix python python-lsp-server gh visual-studio-code
 /opt/homebrew/bin/brew install --cask julia discord skype
 /opt/homebrew/bin/brew install --cask alacritty --no-quarantine
 
