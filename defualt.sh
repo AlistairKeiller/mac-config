fc60c1d893afb328a149f4bce108da4a8ebd6712
Alistair Keiller <43255248+AlistairKeiller@users.noreply.github.com>
2023-07-05 17:37:02 -0700

    add default

---
+++ defualt.sh
@@ -0,0 +1,15 @@
+set -e
+
+# Install Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Install Programs
+/opt/homebrew/bin/brew install rustup-init python
+/opt/homebrew/bin/brew install --cask julia discord skype
+
+# Install Rust
+/opt/homebrew/bin/rustup-init -y --profile complete
+
+# Configure Git
+/opt/homebrew/bin/git config --global user.name "Alistair Keiller"
+/opt/homebrew/bin/git config --global user.email alistair@keiller.net
