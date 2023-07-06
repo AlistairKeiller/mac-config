7e9fab0e1b1e18f9893a7275a55bf85db7fb3046
Alistair Keiller <43255248+AlistairKeiller@users.noreply.github.com>
2023-07-05 18:46:55 -0700

    Update defualt.sh

--- defualt.sh
+++ defualt.sh
@@ -1,5 +1,8 @@
 set -e
 
+# Install Rosetta
+sudo softwareupdate --install-rosetta
+
 # Install Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 (echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/alistairkeiller/.zprofile
