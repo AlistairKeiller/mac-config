7f7f654e59b8f360a3358f7e53e3ba17f900ae0a
Alistair Keiller <43255248+AlistairKeiller@users.noreply.github.com>
2023-07-05 18:46:14 -0700

    Update defualt.sh

--- defualt.sh
+++ defualt.sh
@@ -2,14 +2,12 @@ set -e
 
 # Install Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+(echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/alistairkeiller/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
 
 # Install Programs
 /opt/homebrew/bin/brew install rustup-init python r
-/opt/homebrew/bin/brew install --cask julia discord skype quarto
+/opt/homebrew/bin/brew install --cask julia discord skype visual-studio-code quarto
 
 # Install Rust
 /opt/homebrew/bin/rustup-init -y --profile complete
-
-# Configure Git
-/opt/homebrew/bin/git config --global user.name "Alistair Keiller"
-/opt/homebrew/bin/git config --global user.email alistair@keiller.net
