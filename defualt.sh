0b8ccd0e6fd9e7d282fa5d446e717819858cf6a4
Alistair Keiller <43255248+AlistairKeiller@users.noreply.github.com>
2023-09-23 09:19:17 -0700

    Update defualt.sh

--- defualt.sh
+++ defualt.sh
@@ -10,7 +10,7 @@ eval "$(/opt/homebrew/bin/brew shellenv)"
 
 # Install Programs
 brew install rustup-init python python-lsp-server helix gitui jupyterlab
-brew install --cask julia discord quarto warp
+brew install --cask julia discord quarto warp chromium
 
 # Install Rust
 rustup-init -y --profile complete
