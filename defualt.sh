58cdf5fe74d3fe2e17b69e5fa9fde8af7e0d3f3a
Alistair Keiller <43255248+AlistairKeiller@users.noreply.github.com>
2023-11-05 11:01:24 -0800

    Update defualt.sh

--- defualt.sh
+++ defualt.sh
@@ -1,12 +1,8 @@
+#!/bin/zsh
 set -e
-
-# Install Rosetta
-sudo softwareupdate --install-rosetta
 
 # Install Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-(echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/alistairkeiller/.zprofile
-eval "$(/opt/homebrew/bin/brew shellenv)"
 
 # Install Programs
 brew install rustup-init python helix gitui jupyterlab java
