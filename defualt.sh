2eaa2918150e636794e843ba496fa12b72e6e4ab
Alistair Keiller <43255248+AlistairKeiller@users.noreply.github.com>
2023-11-05 11:03:51 -0800

    Update defualt.sh

--- defualt.sh
+++ defualt.sh
@@ -3,6 +3,8 @@ set -e
 
 # Install Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+(echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/alistairkeiller/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
 
 # Install Programs
 brew install rustup-init python helix gitui jupyterlab java
