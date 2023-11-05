3c698e5497029abd909516fbc8bb6bf307cfed51
Alistair Keiller <43255248+AlistairKeiller@users.noreply.github.com>
2023-11-05 11:05:22 -0800

    Update defualt.sh

--- defualt.sh
+++ defualt.sh
@@ -19,13 +19,6 @@ rustup-init -y --profile complete
 git config --global user.name "Alistair Keiller"
 git config --global user.email alistair@keiller.net
 
-# Configure Alacritty
-mkdir -p ~/.config/alacritty/
-echo 'font:
- size: 15.0
-window:
- option_as_alt: Both'> ~/.config/alacritty/alacritty.yml
-
  # Configure Helix
  mkdir -p ~/.config/helix/
  echo 'theme = "onedark"
