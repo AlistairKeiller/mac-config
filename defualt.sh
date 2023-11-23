2947181b0234196ee7741e14747820f0b3f062ab
Alistair Keiller <43255248+AlistairKeiller@users.noreply.github.com>
2023-11-23 15:55:17 -0700

    Update defualt.sh

--- defualt.sh
+++ defualt.sh
@@ -7,8 +7,8 @@ echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' > /Users/alistairkeiller/.zprof
 eval "$(/opt/homebrew/bin/brew shellenv)"
 
 # Install Programs
-brew install rustup-init python helix gitui jupyterlab java openai-whisper
-brew install --cask --no-quarantine julia discord warp chromium libreoffice modrinth zoom wacom-tablet inkcsape kindle 
+brew install rustup-init python helix gitui jupyterlab java
+brew install --cask --no-quarantine julia discord warp chromium libreoffice modrinth zoom inkcsape kindle 
 
 # FO + Complementary Shaders - Reimagined + Presence Footsteps + AppleSkin + Inventory Profiles Next + Xaero's Minimap + Not Enough Animations + 3D Skin Layers + Roughly Enough Items + AmbientSounds + Xaero's World Map + Shulker Box Tooltip + Visuality + Eating Animation + Mouse Tweaks
 
@@ -36,3 +36,4 @@ git config --global user.email alistair@keiller.net
  hidden = false' > ~/.config/helix/config.toml
 
 # Disable mouse acceleration and natural scrolling
+# Install "Whisper Transcription"
