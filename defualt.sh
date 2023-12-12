3a6d15f39e4b1e37c228c37d4204102a9b737ebd
Alistair Keiller <43255248+AlistairKeiller@users.noreply.github.com>
2023-12-12 10:51:42 -0800

    Update defualt.sh

--- defualt.sh
+++ defualt.sh
@@ -12,11 +12,12 @@ brew install rustup-init python pipx helix gitui jupyterlab font-jetbrains-mono
 brew install --cask --no-quarantine julia discord warp google-chrome libreoffice modrinth zoom inkcsape kindle obs lapce wacom-tablet blender
 
 # FO or SO
+# Mods: Simple Voice Chat + Inventory Management
+# Client Essential Functionality: Shulker Box Tooltip + MacOS Input Fixes + ItemSwapper + Inventory Essentials
+# Client Functional Mods: AppleSkin + Xaero's Minimap (Minimap: off, Deathpoints: off) + Xaero's World Map + Chest Tracker + Detail Armor Bar
+# Client QOL mods: Not Enough Animations + BetterF3 (Font Scale: 0.7) + 3D Skin Layers + Presence Footsteps (Global Volume: 20%) + Eating Animation + Better Ping Display + Chat Heads + Boat Item View + Make Bubbles Pop + ClearDespawn + Highlight + Distant Horizons (incompatible with ModernFix)
 # Shaders: Complementary Shaders - Reimagined + Rethinking Voxels
 # Resource Packs: Default Dark Mode + Enhanced Boss Bars + Even Better Enchants + Fresh Animations
-# Client Functionally Important Mods: AppleSkin + Xaero's Minimap (Minimap: off, Deathpoints: off) + Xaero's World Map + Shulker Box Tooltip + MacOS Input Fixes + ItemSwapper + Chest Tracker + Inventory Essentials
-# Client QOL mods: Not Enough Animations + BetterF3 (Font Scale: 0.7) + 3D Skin Layers + Presence Footsteps (Global Volume: 20%) + Eating Animation + Better Ping Display + Chat Heads + Boat Item View + Make Bubbles Pop + ClearDespawn + Highlight + Distant Horizons (incompatible with ModernFix) + Detail Armor Bar
-# + Simple Voice Chat + Inventory Management
 
 # Install Rust
 rustup-init -y --profile complete
