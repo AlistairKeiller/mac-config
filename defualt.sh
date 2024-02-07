a1c2d04bf5bcbe9437f216794a93deddfe29a661
Alistair Keiller <43255248+AlistairKeiller@users.noreply.github.com>
2024-02-07 15:53:20 -0800

    Update defualt.sh

--- defualt.sh
+++ defualt.sh
@@ -13,7 +13,7 @@ brew install --cask --no-quarantine julia libreoffice modrinth obs zed blender q
 # SO + Simple Voice Chat + Xaero's Minimap (Minimap: off, Deathpoints: off) + Xaero's World Map + Shulker Box Tooltip + Bobby + MacOS Input Fixes + Mouse Wheelie + Zoomify + Librarian Trade Finder + OffersHUD + 
 
 # Install Jupyter
-pip3 install jupyterlab
+pip3 install jupyterlab nbconvert ipyflow
 
 # Install Rust
 rustup-init -y --profile complete
