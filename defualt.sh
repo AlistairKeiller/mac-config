4074ebedbf0037d84f08c04cd2790657ddd65bc6
Alistair Keiller <43255248+AlistairKeiller@users.noreply.github.com>
2024-02-21 17:37:50 -0800

    Update defualt.sh

--- defualt.sh
+++ defualt.sh
@@ -21,7 +21,7 @@ curl -L https://raw.githubusercontent.com/FelixKratz/dotfiles/master/install_ske
 
 # Install Programs
 brew install rustup-init python3
-brew install --cask --no-quarantine julia libreoffice modrinth obs zed blender kdenlive audacity inkscape raspberry-pi-imager mactex-no-gui sidequest alacritty google-chrome lm-studio discord adobe-connect ableton-live-lite blackhole-2ch
+brew install --cask --no-quarantine julia libreoffice modrinth obs zed blender kdenlive audacity inkscape raspberry-pi-imager mactex-no-gui sidequest alacritty google-chrome lm-studio discord adobe-connect
 mkdir -p ~/.config/alacritty/themes
 git clone https://github.com/alacritty/alacritty-theme ~/.config/alacritty/themes
 
