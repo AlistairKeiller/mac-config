5df044c778c5a4a67bd2c1e16cf74cc5f1ff21f8
Alistair Keiller <43255248+AlistairKeiller@users.noreply.github.com>
2024-03-14 18:26:25 -0700

    Update defualt.sh

--- defualt.sh
+++ defualt.sh
@@ -37,6 +37,9 @@ rustup-init -y --profile complete
 git config --global user.name "Alistair Keiller"
 git config --global user.email alistair@keiller.net
 
+# Configure ssh
+echo "ServerAliveInterval 60" > ~/.ssh/config
+
 # Disable mouse acceleration, natural scrolling, and shake mouse pointer to locate
 # Automatically hide the menu bar and dock
 # install "Aiko", 3Dconnexion, and Focusrite Control 2
