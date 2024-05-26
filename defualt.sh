afb8df7a8a040d84f8b157732a48a24f5b93b7a5
Alistair Keiller <alistair@keiller.net>
2024-05-25 19:59:11 -0700

    amethyst

--- defualt.sh
+++ defualt.sh
@@ -1,5 +1,4 @@
 #!/bin/zsh
-set -e
 
 # Install Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
@@ -12,22 +11,13 @@ sudo sh -c 'echo /opt/homebrew/bin/fish >> /etc/shells'
 chsh -s /opt/homebrew/bin/fish
 fish -c "fish_add_path /opt/homebrew/bin"
 
-# Tiling window management
-brew install FelixKratz/formulae/borders koekeishiya/formulae/yabai koekeishiya/formulae/skhd
-echo "$(whoami) ALL=(root) NOPASSWD: sha256:$(shasum -a 256 $(which yabai) | cut -d " " -f 1) $(which yabai) --load-sa" | sudo tee /private/etc/sudoers.d/yabai
-skhd --start-service
-yabai --start-service
-brew services start borders
-curl -L https://raw.githubusercontent.com/FelixKratz/dotfiles/master/install_sketchybar.sh | sh
-cp -r mac-config/config/* .config/
-# https://github.com/koekeishiya/yabai/wiki/Disabling-System-Integrity-Protection
-
 # Install Programs
 brew install rustup-init
-brew install --cask --no-quarantine julia zed obs alacritty google-chrome discord spotify bambu-studio mactex-no-gui kdenlive
-
-# Install Rust
+brew install --cask --no-quarantine julia zed obs alacritty google-chrome discord spotify bambu-studio kdenlive amethyst
+curl -L https://raw.githubusercontent.com/FelixKratz/dotfiles/master/install_sketchybar.sh | sh
 rustup-init -y --profile complete
+# https://ianyh.com/amethyst/
+# keyboard shortcuts -> mission control -> mission control -> switch to desktops
 
 # Configure Git
 git config --global user.name "Alistair Keiller"
@@ -36,6 +26,9 @@ git config --global user.email alistair@keiller.net
 # Configure ssh
 mkdir -p ~/.ssh
 echo "ServerAliveInterval 60" > ~/.ssh/config
+
+# .config
+cp -r mac-config/config/* .config/
 
 # Disable mouse acceleration, natural scrolling, and shake mouse pointer to locate
 # Automatically hide the menu bar and dock
