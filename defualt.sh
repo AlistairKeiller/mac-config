a238938d46afb3844662c2c7cfa4d5b35591d413
Alistair Keiller <43255248+AlistairKeiller@users.noreply.github.com>
2024-06-18 11:41:56 -0700

    Update defualt.sh

--- defualt.sh
+++ defualt.sh
@@ -12,8 +12,9 @@ chsh -s /opt/homebrew/bin/fish
 fish -c "fish_add_path /opt/homebrew/bin"
 
 # Install Programs
-brew install rustup-init gh python pipx
-brew install --cask --no-quarantine julia zed obs alacritty google-chrome discord spotify bambu-studio kdenlive amethyst
+brew tap oven-sh/bun
+brew install rustup-init gh python pipx bun
+brew install --cask --no-quarantine julia zed obs alacritty chromium discord spotify bambu-studio kdenlive amethyst
 curl -L https://raw.githubusercontent.com/FelixKratz/dotfiles/master/install_sketchybar.sh | sh
 rustup-init -y --profile complete
 # https://ianyh.com/amethyst/
