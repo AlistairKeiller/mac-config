caee31661e895bd1907886d766eb06ef3757d5ea
Alistair Keiller <43255248+AlistairKeiller@users.noreply.github.com>
2024-08-07 20:28:34 -0700

    Update defualt.sh

--- defualt.sh
+++ defualt.sh
@@ -13,7 +13,7 @@ fish -c "fish_add_path /opt/homebrew/bin"
 
 # Install Programs
 brew tap oven-sh/bun
-brew install rustup-init gh python pipx bun
+brew install rustup-init trunk gh python pipx bun
 brew install --cask --no-quarantine julia zed obs alacritty chromium discord spotify bambu-studio kdenlive autodesk-fusion nikitabobko/tap/aerospace inkscape visual-studio-code
 rustup-init -y --profile complete
 
