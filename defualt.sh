c999d8ebd9f9509bf85f7f9c779338a63bb21db8
Alistair Keiller <alistair@keiller.net>
2024-09-07 23:32:25 -0700

    test

--- defualt.sh
+++ defualt.sh
@@ -53,5 +53,5 @@ defaults write -g NSWindowShouldDragOnGesture YES
 
 # Install 3DxWare and Fusion
 # Theme zed (extension), chrome (web store)
-# Disable mouse acceleration, natural scrolling, and shake mouse pointer to locate
+# Auto hide dock, disable mouse acceleration, natural scrolling, and shake mouse pointer to locate
 # Automatically hide the menu bar and dock
