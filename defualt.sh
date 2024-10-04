427357371329632688fb07a0b976e75ed207b405
Alistair Keiller <43255248+AlistairKeiller@users.noreply.github.com>
2024-10-03 21:50:39 -0700

    Update defualt.sh

--- defualt.sh
+++ defualt.sh
@@ -51,6 +51,6 @@ cp -r ./config/* ~/.config/
 # ctrl+cmd to drag window
 defaults write -g NSWindowShouldDragOnGesture YES
 
-# Install 3DxWare and Fusion
+# Install 3DxWare, Fusion, and LockDown Browser
 # Theme zed (extension), chrome (web store)
 # Auto hide dock, disable mouse acceleration, natural scrolling, and shake mouse pointer to locate
